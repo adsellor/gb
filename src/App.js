f9371728533de17536a5e884186b48c1f0b09672
Zaven <adsellor@protonmail.com>
2019-07-25 13:27:44 +0400

    Ehm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-card">
+          <p className="App-news">Unconfirmed*</p>
+        </div>
+        <p> The last appearance of the great leader was on July 5 </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className="App-small"
+          href="https://web.archive.org/web/20190721141702/https://m.gazeta.ru/politics/news/2019/07/21/n_13242091.shtml"
         >
-          Learn React
+          {" "}
+          *Denied officially{" "}
         </a>
       </header>
     </div>
